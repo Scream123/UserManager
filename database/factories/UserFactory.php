13d3f3760bfbb70341a297b41d8e8d58379c1ad0
Volodymyr Marakhovskyi <fly_life@mail.ru>
2025-04-01 15:01:00 +0300

    added User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,10 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -24,11 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => '+380' . $this->faker->unique()->numberBetween(100000000, 999999999),
+            'position_id' => $this->faker->numberBetween(1, 5),
+            'photo' => $this->faker->imageUrl(70, 70, 'people', true, 'jpg'),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
